d23385ee7d9cf32c621303c320826d5fdb075d8e
Koson Pengphipat <67030030@kmitl.ac.th>
2024-11-19 13:30:04 +0700

    Update 1.3

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,4 +1,4 @@
 #1.3
-a = "Koson"
-b = "Pengphipat"
-print(f" Hey, what's your first name? : {a} \n And your last name? : {b} \n Well, Pleased to meet you, {a} {b}")
+a = input("Hey, what's your first name? :")
+b = input("And your last name? : ")
+print(f"Well, Pleased to meet you, {a} {b}")
