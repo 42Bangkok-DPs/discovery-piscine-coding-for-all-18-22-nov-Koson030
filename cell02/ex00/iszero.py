d1ae4da91d2c56b05aa06aa9a758ba45a0e920f6
Koson Pengphipat <67030030@kmitl.ac.th>
2024-11-19 13:19:22 +0700

    exercise0.py

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+#รับค่าตัวเลขจากผู้ใช้
+a = float(input("Enter Number : "))
+
+#ตรวจสอบว่ามากกว่า 0 หรือเท่ากับ 0
+if a > 0:
+    print("This number is different from zero.")
+elif a == 0:
+    print("This number is equal to zero.")
