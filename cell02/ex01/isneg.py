5f31cf3d501de4f4277334ad8ad1d93db17b8213
Koson Pengphipat <67030030@kmitl.ac.th>
2024-11-20 09:02:39 +0700

    exercise1.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+#2.1
+a = float(input("Enter Number : "))
+
+if a > 0:
+    print("This number is positive.")
+elif a < 0 :
+    print("This number is negative.")
+elif a == 0 :
+    print("This number is both positive and negative.")
