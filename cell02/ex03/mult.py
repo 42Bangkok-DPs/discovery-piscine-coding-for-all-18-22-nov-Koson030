cd18405d005f196ad292c27d9290d7e5114b5b5e
Koson Pengphipat <67030030@kmitl.ac.th>
2024-11-20 09:25:27 +0700

    exercise3.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,16 @@
+#2.3
+# รับค่าจากผู้ใช้
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+
+# คูณและแสดงผล
+x = a * b
+
+# ตรวจสอบผลลัพธ์
+print(f" {a} x {b} = {x}")
+if x > 0 :
+    print("The result is positive.")
+elif x < 0 :
+    print("The result is negative.")
+elif x == 0 :
+    print("The result is positive and negative")
