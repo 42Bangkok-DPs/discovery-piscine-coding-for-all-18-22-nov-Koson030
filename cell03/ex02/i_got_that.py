665744c1668ddb431fa1ea4ff86e34df247416a7
Koson Pengphipat <67030030@kmitl.ac.th>
2024-11-20 16:57:15 +0700

    Update

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,6 +1,7 @@
 #3.2
-a = input("What you gotta say?: ")
-while True :
+a = input("What you gotta say?: ") # รับข้อความแรกจากผู้ใช้
+
+while True : # ลูปถามต่อไปจนกว่าจะพิมพ์ "STOP"
     b = input("I got that! else?: ")
-    if b == "STOP":
+    if b == "STOP":  # ถ้าพิมพ์ "STOP" จะหยุดลูป
         break
