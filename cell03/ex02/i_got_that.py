eefe108d3886d1f5455f1a368833f21315cfdcac
Koson Pengphipat <67030030@kmitl.ac.th>
2024-11-20 16:55:26 +0700

    Update

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,6 +1,6 @@
 #3.2
-word1 = input("What you gotta say?: ")
+a = input("What you gotta say?: ")
 while True :
-    word2 = input("I got that! else?: ")
-    if word2 == "STOP":
+    b = input("I got that! else?: ")
+    if b == "STOP":
         break
