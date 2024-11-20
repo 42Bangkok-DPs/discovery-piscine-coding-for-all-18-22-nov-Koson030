838a8557cdd479f04357b4ecbe0e0d7786961a0d
Koson Pengphipat <67030030@kmitl.ac.th>
2024-11-20 17:06:30 +0700

    Update

--- cell02/ex02/password.py
+++ cell02/ex02/password.py
@@ -3,10 +3,10 @@
 password = "Python is awesome"
 
 # ขอให้ผู้ใช้ป้อนรหัสผ่าน
-user_password = input("Please enter your password: ")
+password1 = input("Please enter your password: ")
 
 # ตรวจสอบรหัสผ่านที่ผู้ใช้ป้อน
-if user_password == password:
+if password1 == password:
     print("ACCESS GRANTED")
 else:
     print("ACCESS DENIED")
