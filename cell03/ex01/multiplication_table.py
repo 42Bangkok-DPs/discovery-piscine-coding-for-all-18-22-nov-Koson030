2a24c36e32ef54af266b3a8f294f575bb3ae2f16
Koson Pengphipat <67030030@kmitl.ac.th>
2024-11-20 17:57:44 +0700

    Update 3.1

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,4 +1,4 @@
 #3.1
-a = int(input("Enter a number : ")) #รับข้อมูลทางคีย์บอร์ด 
+a = int(input("Enter a number \n")) #รับข้อมูลทางคีย์บอร์ด 
 for i in range(1,10): #วนลูปตั้งแต่ 1 ถึง 9
     print(f" {i} x {a} = {a*i}") #แสดงผลคูณ
