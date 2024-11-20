7351cb794d2b2da75fb77aa7648036c5e2f1a3c9
Koson Pengphipat <67030030@kmitl.ac.th>
2024-11-20 16:48:28 +0700

    Update

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,7 +1,7 @@
 #3.2
 while True:
-    user_input = input("What you gotta say? : ")   # รับข้อความจากคีบอร์ด
-    if user_input == "STOP": # ตรวจสอบว่าป้อน "STOP" หรือไม่
+    a = input("What you gotta say? : ")   # รับข้อความจากคีบอร์ด
+    if a == "STOP": # ตรวจสอบว่าป้อน "STOP" หรือไม่
         break  # ออกจากลูปเมื่อพิมพ์ STOP
-    print(f"I got that! Anything else? : {user_input}")  # แสดงข้อความตอบกลับเมื่อใส่คำที่ไม่ใช่stop
+    print(f"I got that! Anything else? : {at}")  # แสดงข้อความตอบกลับเมื่อใส่คำที่ไม่ใช่stop
 
